9586c5d6a5afc17e3add71a50bfa8b11ed16f1ea
Ryan Seys <ryan@ryanseys.com>
2015-03-30 15:32:07 -0400

    Modularize

--- main.rb
+++ main.rb
@@ -1,11 +1,106 @@
-# Constants to change
-MAX_TIME = 50000
-MAX_REPETITIONS = 20
-NUM_QUEUES = 5
-NUM_SERVERS = 1
-CASE = 1
-SYMMETRIC = true
-POLICY = :random
+# Constants - Do not change
+POLICIES = {
+  :random => true,
+  :roundrobin => true,
+  :lcq => true
+}
+
+# Calculates the average of all the numbers in the array
+def average(arr)
+  arr.reduce(:+).to_f / arr.size
+end
+
+class SimulationTopologyOne
+  def initialize(maxTime, maxReps, numQueues, numServers, policy, isSymmetric)
+    @maxTime = maxTime
+    @maxReps = maxReps
+    @numQueues = numQueues
+    @numServers = numServers
+    @isSymmetric = isSymmetric
+    @policy = policy
+
+    @time = 0
+    @queues = []
+    @servers = []
+
+    lambda = 0.02
+    probability = 1
+
+    # Create N queues
+    for i in (0..@numQueues)
+      @queues[i] = SimQueue.new(lambda, probability)
+    end
+
+    for i in (0..@numServers)
+      @servers[i] = Server.new
+    end
+
+    @stats = {
+      :numServiced => 0,
+      :averageQueueLengths => []
+    }
+  end
+
+  def generateArrivals()
+    for i in (0..@numQueues)
+      queue = @queues[i]
+      queue.generateArrival(@time)
+    end
+  end
+
+  def printStatistics
+    puts "Average queue length: #{average(@stats[:averageQueueLengths])}"
+  end
+
+  # Selects a queue based on the different policies possible
+  def selectQueue(policy)
+    if policy == :random
+      # Return queue with max length
+      return (@queues.select { |q| q.connected? }).max_by(&:size)
+    elsif policy == :roundrobin
+      puts 'Policy Round Robin not implemented.'
+      raise
+    elsif policy == :lcq
+      puts 'Policy LCQ not implemented.'
+      raise
+    end
+  end
+
+  def collectStatistics
+    queueLengths = @queues.map { |q| q.size }
+    averageLength = average(queueLengths)
+    @stats[:averageQueueLengths].push(averageLength)
+  end
+
+  def runSimulation
+    while @time <= @maxTime
+      # Check if policy exists
+      if not POLICIES.member?(@policy)
+        puts 'Invalid policy!'
+        break
+      end
+
+      # Step 1. Select a queue given the policy set
+      queue = selectQueue(@policy)
+
+      # Step 2. Server serves the head packet in the queue.
+      if queue.nil?
+        # puts 'No queue could be selected.'
+      else
+        packet = queue.deq()
+        @servers[0].process(packet) # Use only first server
+        @stats[:numServiced] += 1
+      end
+
+      # Step 3. New packet arrivals are added to the queues.
+      generateArrivals()
+
+      collectStatistics()
+
+      @time += 1
+    end
+  end
+end
 
 # SimQueue represents a Queue for
 # the purposes of this simulation.
@@ -63,87 +158,8 @@ class Server
   end
 end
 
-time = 0
-@queues = []
+# Run your simulations here
 
-policies = {
-  :random => true,
-  :roundrobin => true,
-  :lcq => true
-}
-
-server = Server.new
-numServiced = 0
-probability = 0.2 # queue n is connected with probability pn i.e., E[Cn(t)] = pn.
-lambda = 0.03
-
-# Selects a queue based on the different policies possible
-def selectQueue(policy)
-  if policy == :random
-    # Return queue with max length
-    return (@queues.select { |q| q.connected? }).max_by(&:size)
-  elsif policy == :roundrobin
-    puts 'Policy Round Robin not implemented.'
-    raise
-  elsif policy == :lcq
-    puts 'Policy LCQ not implemented.'
-    raise
-  end
-end
-
-def generateArrivals(time)
-  for i in (0..NUM_QUEUES)
-    queue = @queues[i]
-    queue.generateArrival(time)
-  end
-end
-
-@averageLengths = []
-
-def average(arr)
-  arr.reduce(:+).to_f / arr.size
-end
-
-def collectStatistics
-  queueLengths = @queues.map { |q| q.size }
-  averageLength = average(queueLengths)
-  @averageLengths.push(averageLength)
-end
-
-def printStatistics
-  puts "Average queue length: #{average(@averageLengths)}"
-end
-
-# Create N queues
-for i in (0..NUM_QUEUES)
-  @queues[i] = SimQueue.new(lambda, probability)
-end
-
-while time <= MAX_TIME
-  # Check if policy exists
-  if not policies.member?(POLICY)
-    puts 'Invalid policy!'
-    break
-  end
-
-  # Step 1. Select a queue given the policy set
-  queue = selectQueue(POLICY)
-
-  # Step 2. Server serves the head packet in the queue.
-  if queue.nil?
-    # puts 'No queue could be selected.'
-  else
-    packet = queue.deq()
-    server.process(packet)
-    numServiced += 1
-  end
-
-  # Step 3. New packet arrivals are added to the queues.
-  generateArrivals(time)
-
-  collectStatistics()
-
-  time += 1
-end
-
-printStatistics()
+sim1 = SimulationTopologyOne.new(1000, 20, 5, 1, :random, true)
+sim1.runSimulation
+sim1.printStatistics
